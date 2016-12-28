c53601940c8647fbadc8bfbc5c3e751f28d69903
bdauer <dauer.benjamin@gmail.com>
2016-12-28 11:58:27 -0500

    Add profile model with save_last_login method

--- tasktracker/userprofiles/models.py
+++ tasktracker/userprofiles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.signals import user_logged_out
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    most_recent_login = models.DateField()
 
 # Create your models here.
+
+    @receiver(user_logged_out, sender)
+    def save_last_login(sender, request, user):
+        self.most_recent_login = user.last_login
+        self.save()
