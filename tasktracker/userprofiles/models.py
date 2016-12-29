cc062d59b507328d5fcc99a3cf3570edb8d10b90
bdauer <dauer.benjamin@gmail.com>
2016-12-29 09:13:03 -0500

    Add Profile model with receiver for tracking last login: would otherwise get overwritten on login.

--- tasktracker/userprofiles/models.py
+++ tasktracker/userprofiles/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.signals import user_logged_out
+from django.dispatch import receiver
+from django.utils import timezone
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    most_recent_login = models.DateField()
+    most_recent_login = models.DateField(default=timezone.now)
 
 # Create your models here.
 
-    @receiver(user_logged_out, sender)
-    def save_last_login(sender, request, user):
-        self.most_recent_login = user.last_login
-        self.save()
+    @receiver(user_logged_out, sender=User)
+    def save_last_login(sender, request, user, **kwargs):
+        profile = user.profile
+        profile.most_recent_login = user.last_login
+        profile.save()
