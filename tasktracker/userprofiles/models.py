8bad1b36184469d5b3121e3c2bbcca136681fc59
bdauer <dauer.benjamin@gmail.com>
2017-01-24 10:35:27 -0500

    Add listeners for creating new profile when new user is created

--- tasktracker/userprofiles/models.py
+++ tasktracker/userprofiles/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.signals import user_logged_out
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
 
@@ -8,10 +9,17 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     most_recent_login = models.DateField(default=timezone.now)
 
-# Create your models here.
+@receiver(user_logged_out, sender=User)
+def save_last_login(sender, request, user, **kwargs):
+    profile = user.profile
+    profile.most_recent_login = user.last_login
+    profile.save()
 
-    @receiver(user_logged_out, sender=User)
-    def save_last_login(sender, request, user, **kwargs):
-        profile = user.profile
-        profile.most_recent_login = user.last_login
-        profile.save()
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
